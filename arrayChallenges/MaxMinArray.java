1b225eefd1b4a6320a2c8b6dddd7a8037382fea2
Anurag Pandey <panurag8127@gmail.com>
2025-03-19 21:46:16 +0530

    done

--- arrayChallenges/MaxMinArray.java
+++ arrayChallenges/MaxMinArray.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class MaxMinArray {
     public static void main(String[] args) {
-         Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Please enter the number of elements : ");
         int size = input.nextInt();
         int[] nums = new int[size];
@@ -12,5 +12,34 @@ public class MaxMinArray {
             nums[i] = input.nextInt();
             i++;
         }
+        int max = max(nums);
+        int min = min(nums);
+        System.out.println("The maximum number in the array is : "+ max);
+        System.out.println("The minimum number in the array is : "+ min);
+    }
+    public static int max(int[]nums){
+        if(nums.length == 0){
+            return Integer.MIN_VALUE;
+        }
+        int max = nums[0];
+        int i = 0;
+        while(i<nums.length){
+            if(max<nums[i]){
+                max = nums[i];
+            }
+            i++;
+        }
+        return max;
+    }
+    public static int min(int[]nums){
+        int min = Integer.MAX_VALUE;
+        int i =0;
+        while(i<nums.length){
+            if(min>nums[i]){
+                min = nums[i];
+            }
+            i++;
+        }
+        return min;
     }
 }
