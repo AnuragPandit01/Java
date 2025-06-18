24d12c0212a4a0f75df640b9ecc0c9535a4a7ac1
Anurag Pandey <panurag8127@gmail.com>
2025-06-18 09:36:42 +0530

    changes

--- filehandling/WriteToFile.java
+++ filehandling/WriteToFile.java
@@ -6,7 +6,7 @@ public class WriteToFile {
     public static void main(String[] args) {
         try{
             FileWriter myWriter = new  FileWriter("myFile.txt");
-            myWriter.write("Files in Java might be tricky , but its a fun");
+            myWriter.write("Files in Java might be tricky , but its a fun in learning");
             myWriter.close();
             System.out.println("Succesfully wrote in the file");
         }catch(IOException e){
